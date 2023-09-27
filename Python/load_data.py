6d7c80f9c823422734bba7ee77741931504a4294
ilmari99 <i.vahteristo@gmail.com>
2023-09-27 09:23:28 +0300

    Added train_test_validation_sequence_split

--- Python/load_data.py
+++ Python/load_data.py
@@ -32,11 +32,32 @@ def separate_test_set(path, sz_percent=0.12):
     df.to_csv(path[:-4] + "_train.csv", index=False)
     df_test.to_csv(path[:-4] + "_test.csv", index=False)
     
+def train_test_validation_split_sequence(path,keep_stagnant = False, validation_size=0.2):
+    """ Load the dataframe and drop the first two days.
+    Take the test set as all data upto '2017-05-13'
+    IF not keep_stagnant, drop the data from '2017-05-13' to '2017-06-15'
+    Take the validation set as the last 20% of the training data.
+    """
+    df = load_to_dataframe(path, remove_first_days=True)
+    test_df = df[df['date'] < '2017-05-13']
+    df = df[df['date'] > '2017-05-13']
+    if not keep_stagnant:
+        df = df[df['date'] > '2017-06-15']
+    # Take the last 20% of dates as validation set
+    unique_dates = df['date'].unique()
+    split_idx = int(len(unique_dates)*(1-validation_size))
+    validation_dates = unique_dates[split_idx:]
+    validation_df = df[df['date'].isin(validation_dates)]
+    df = df[~df['date'].isin(validation_dates)]
+    return df, test_df, validation_df
+    
+    
     
 def remove_sus_period(df):
     """ Remove data from 2017-05-13 to 2017-06-15
     """
-    df = df[df['date'] < '2017-05-13' or df['date'] > '2017-06-15']
+    df = df[(df['date'] < '2017-05-13') | (df['date'] > '2017-06-15')]
+    return df
 
 if __name__ == "__main__":
     separate_test_set("miningdata/data.csv", sz_percent=0.12)
